d7d7f7af050b099a662b3f9371f3fac52c37b6a1
Mucunshuo <141752057+Mucunshuo@users.noreply.github.com>
2024-12-15 17:17:01 +0800

    Create models.py

---
+++ Suwen/suwen/agent/models.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+
+from lagent.llms import (GPTAPI, INTERNLM2_META, HFTransformerCasualLM,
+                         LMDeployClient, LMDeployServer)
+
+load_dotenv()
+
+internlm_server = dict(type=LMDeployServer,
+                       path='/root/InternLM/XTuner/merged01',
+                       model_name='internlm2',
+                       meta_template=INTERNLM2_META,
+                       top_p=0.8,
+                       top_k=1,
+                       temperature=0,
+                       max_new_tokens=8192,
+                       repetition_penalty=1.02,
+                       stop_words=['<|im_end|>'])
+
+internlm_client = dict(type=LMDeployClient,
+                       model_name='internlm2_5-7b-chat',
+                       url='http://127.0.0.1:23333',
+                       meta_template=INTERNLM2_META,
+                       top_p=0.8,
+                       top_k=1,
+                       temperature=0,
+                       max_new_tokens=8192,
+                       repetition_penalty=1.02,
+                       stop_words=['<|im_end|>'])
+
+internlm_hf = dict(type=HFTransformerCasualLM,
+                   path='internlm/internlm2_5-7b-chat',
+                   meta_template=INTERNLM2_META,
+                   top_p=0.8,
+                   top_k=None,
+                   temperature=1e-6,
+                   max_new_tokens=8192,
+                   repetition_penalty=1.02,
+                   stop_words=['<|im_end|>'])
+
+                        temperature=0,
+                        max_new_tokens=8192,
+                        repetition_penalty=1.02,
+                        stop_words=['<|im_end|>'])
